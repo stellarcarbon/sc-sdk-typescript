55ba5ded1190c849a794ce439c0bfa095c0ac045
Hendrik Grondijs <hlgrondijs@gmail.com>
2025-07-09 13:33:01 +0200

    oops

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from '@hey-api/openapi-ts';
+import { defineConfig, UserConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
   input: 'https://api.stellarcarbon.io/openapi.json',
   output: 'src/generated',
-     parser: { 
-     filters: { 
-       deprecated: false, 
-     }, 
-   },
-});
+  parser: {
+    filters: {
+      deprecated: false,
+    },
+  },
+} as UserConfig);
